3ffe2fc716aacfde5988cba4cf800f37a4392af2
unknown <bons6710hos@gmail.com>
2025-05-20 13:51:09 +0300

    basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+# Addition
+result_addition = number1 + number2
+# Subtraction
+result_subtraction = number1 - number2
+# Multiplication
+result_multiplication = number1 * number2
+
+# print("Addition:", result_addition)
+
+
+
+print("Addition OF", number1 ,"and ", number2 ," is ", result_addition)
+print("Subtraction OF", number1 ,"and ", number2 ," is ", result_subtraction)
+print("Multiplication OF", number1 ,"and ", number2 ," is ", result_multiplication)
